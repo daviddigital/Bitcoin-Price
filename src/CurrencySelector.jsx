ca42372bba7588daf0690f1526ed306eb1bd942a
David Watts <davidwatts11@gmail.com>
2022-01-12 10:18:48 +1000

    added currency selector

--- src/CurrencySelector.jsx
+++ src/CurrencySelector.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 
-
+// Todo, use v1/bpi/supported-currencies.json to get list of currencies. Iterate through or map to options. 
 
 export default function CurrencySelector( {currency, setCurrency} ) {
 
